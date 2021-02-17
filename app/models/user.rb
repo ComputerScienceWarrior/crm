1c62978a655cefc5f99f78f185c9eaf63d42b1a7
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-17 15:07:47 -0700

    Add associations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    belongs_to :company
+    has_many :notes
+    has_many :cases
+    has_many :clients, through: :notes
+    has_many :clients, through: :cases
 end
