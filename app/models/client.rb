45fc72583fdf3938ed79dd73609d61cfcb2d6ed1
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-17 15:12:36 -0700

    Add associations to Client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,8 @@
 class Client < ApplicationRecord
+    belongs_to :company
+    has_many :cases
+    has_many :notes
+    has_many :client_contacts
+    has_many :users, through: :notes
+    has_many :users, through: :cases
 end
