421b88e9a650d9b582e33ff7ee0f88da9604ae45
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-17 15:06:51 -0700

    Revert "Add relationships to Company"
    This reverts commit 31af923b4f453f42b85ffcaceede2d2b7bddabcd.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,2 @@
 class User < ApplicationRecord
-    belongs_to :company
-    has_many :notes
-    has_many :cases
-    has_many :clients, through: :notes
-    has_many :clients, through: :cases
 end
