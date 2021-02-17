6950a2fd8ab58f9015266d1e58cb8b06edb18170
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-17 15:03:56 -0700

    Add relationships to Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,7 @@
 class Company < ApplicationRecord
+    has_many :clients
+    has_many :users
+    has_many :client_contacts, through: :clients
+    has_many :notes, through: :users
+    has_many :cases, through: :users
 end
