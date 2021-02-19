c0567033bfb705362f291e598144e17336366aca
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-19 14:16:37 -0700

    create 'index, show, new, create, edit update and destroy' actions

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,30 @@
 class UserController < ApplicationController
+    
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        # logic to create a user
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        # logic to update a users information
+    end
+
+    def destroy
+        # logic to destroy a user and their associated resources
+    end
 end
