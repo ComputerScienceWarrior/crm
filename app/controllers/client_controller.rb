6c5fc91399ce9193fb78d1ed7014fef8f86b3ab6
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-19 14:25:02 -0700

    create client controller actions 'index, show, new, edit, update, detroy and create.'

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,2 +1,30 @@
 class ClientController < ApplicationController
+
+    def index
+        @clients = Client.all
+    end
+
+    def show
+        @client = Client.find(params[:id])
+    end
+
+    def new
+        @client = Client.new
+    end
+
+    def create
+        # logic to create a Client
+    end
+
+    def edit
+        @client = Client.find(params[:id])
+    end
+
+    def update
+        # logic to update client information
+    end
+
+    def destroy
+        # logic to destroy a client and it's associations
+    end
 end
