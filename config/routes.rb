3a54936e38b1049cd032636885bb24f9de2cc905
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-19 14:01:53 -0700

    create routes for: (user, company, client, client_contact, note and case) models.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
+
+  # STATIC ROUTES
   root "static#home"
 
-  # Login
+  # SESSION ROUTES
   get '/login', to: "session#login"
 
+  # USER ROUTES
+  resources :user
+  # COMPANY ROUTES
+  resources :company
+  # CLIENT ROUTES
+  resources :client
+  # CLIENT_CONTACT ROUTES
+  resources :client_contact
+  # NOTE ROUTES
+  resources :note
+  # CASE ROUTES
+  resources :case
 end
