af212faf0d8b83fdd70af582c321e89334524568
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-19 14:21:24 -0700

    create 'show, new, create, edit, update and destroy' actions.

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,2 +1,27 @@
 class CompanyController < ApplicationController
+    # does it makes sense to have an index of companies? Probably not. Maybe for Admin routes, but not stadard user routes
+    # create a 'home route'
+    def show
+        @company = Company.find(params[:id])
+    end
+
+    def new
+        @company = Company.new
+    end
+
+    def create
+        # logic to create a new company
+    end
+
+    def edit
+        @company = Company.find(params[:id])
+    end
+
+    def update
+        # logic to update a companies information
+    end
+
+    def destroy
+        # logic to destroy a company and their resources
+    end
 end
