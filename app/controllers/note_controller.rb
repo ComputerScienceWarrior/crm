425a0bbf6c644320e60f7266c6193799e71e4f18
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-21 09:44:41 -0700

    create 'new' and 'create' actions.

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -7,5 +7,12 @@ class NoteController < ApplicationController
         @note = Note.find(params[:id])
     end
 
-    
+    def new
+        @note = Note.new
+    end
+
+    def create
+        # logic to create a new note
+    end
+
 end
