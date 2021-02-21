6d40a63b17d7f28a2e159ad730ce5ceb7ce5c299
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-21 09:55:32 -0700

    create actions for 'edit', 'update', 'destroy'.

--- app/controllers/case_controller.rb
+++ app/controllers/case_controller.rb
@@ -14,4 +14,16 @@ class CaseController < ApplicationController
     def create 
         # logic to create a new case
     end
+
+    def edit
+        @case = Case.find(params[:id])
+    end
+
+    def update
+        # logic to update controller
+    end
+
+    def destroy
+        # logic to destroy a case
+    end
 end
