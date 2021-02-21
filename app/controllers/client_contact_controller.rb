b80478fdaeb33004cf179c488808d8419e3ab179
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-21 09:40:02 -0700

    Create 'destroy, edit, update, index, show, new and create' actions'

--- app/controllers/client_contact_controller.rb
+++ app/controllers/client_contact_controller.rb
@@ -1,2 +1,29 @@
 class ClientContactController < ApplicationController
+    def index
+        @client_contacts = ClientContact.all
+    end
+
+    def show
+        @client_contact = ClientContact.find(params[:id])
+    end
+
+    def new
+        @client_contact = ClientContact.new 
+    end
+
+    def create
+        # logic for creating a client_contact
+    end
+
+    def edit
+        @client_contact = ClientContact.find(params[:id])
+    end
+
+    def update
+        # logic to update a client_contact
+    end
+
+    def destroy
+        # logic to destroy a resource
+    end
 end
