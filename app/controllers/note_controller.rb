c188002fad5d14b5b46d85e180a6682a0d042300
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-21 09:48:19 -0700

    Create 'edit' 'update' and 'destroy' actions.

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -15,4 +15,16 @@ class NoteController < ApplicationController
         # logic to create a new note
     end
 
+    def edit 
+        @note = Note.find(params[:id])
+    end
+
+    def update
+        # logic to update a note
+    end
+
+    def destroy
+        # logic to destroy a note and it's associated resources.
+    end
+
 end
