e66b89f400332bdbfcd4eceb69f974d1df7b0030
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-22 12:18:14 -0700

    add logic to create a user. Create user_params private method

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,6 +7,19 @@ class SessionController < ApplicationController
     end
 
     def create
-        # logic to create a user
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id 
+            redirect_to root
+        else
+            render :new
+        end
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :password, :password_conf, :email, :company_id)
     end
 end
