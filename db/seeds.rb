f355c31c64fe764fb081c7c3a79c3976851a0bf3
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-22 12:53:43 -0700

    create seed data for a Company.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+company_names = ["Toys-R-I", "CPC", "init-0-edu", "Slicez Barber Shop"]
+industries = ["Toy", "Medical", "Education", "Services"]
+4.times do |index|
+    Company.create(name: company_names[index], industry: industries[index], size: 10, price_plan: "Middle")
+end
