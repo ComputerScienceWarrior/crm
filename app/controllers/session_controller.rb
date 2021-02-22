c8bef74949fb5adf83aaa2c51ea1dbde9b8bd6da
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-21 17:06:23 -0700

    Revert "Add 'new' and"
    This reverts commit 9297514f4ec88e7be4220e9f95057bf4f8841bfc.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,12 +1,4 @@
 class SessionController < ApplicationController
     def login
     end
-
-    def new
-        @user = User.new
-    end
-
-    def create
-        # logic to create a user
-    end
 end
