57c8e0595924e799b88c328519469e73a648f6e7
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-21 17:06:49 -0700

    add 'create' and 'new' actions

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,12 @@
 class SessionController < ApplicationController
     def login
     end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        # logic to create a user
+    end
 end
