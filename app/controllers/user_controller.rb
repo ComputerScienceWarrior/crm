bbfd3dd1f42e632083bb7a1f6b95d11288986725
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-23 11:47:07 -0700

    change from ':password_conf' to ':password_confirmation'

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -36,6 +36,6 @@ class UserController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :username, :password, :password_conf, :email, :company_id)
+        params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation, :email, :company_id)
     end
 end
