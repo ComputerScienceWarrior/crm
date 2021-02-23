f84e32127dd6854c47e7fe188560cabf759423ad
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-23 09:19:38 -0700

    remove 'create' action

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,18 +1,16 @@
 class SessionController < ApplicationController
-    def login
-    end
-
+    
     def new
         @user = User.new
     end
-
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id 
-            redirect_to root
+    
+    def login
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to root_path
         else
-            render :new
+            render :login
         end
     end
 
