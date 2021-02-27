b97e144bec1afa354e9157e7e0ada11405cff9b1
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-27 05:35:46 -0700

    create before_action to find_user for all actions in DRY way. add 'update' logic.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,11 @@
 class UserController < ApplicationController
-    
+    before_action :find_user, only: [:index, :show, :edit, :update, :destroy]
+
     def index
         @users = User.all
     end
 
     def show
-        @user = User.find(params[:id])
     end
 
     def new
@@ -22,20 +22,27 @@ class UserController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        # logic to update a users information
+        if @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
     end
 
     def destroy
-        # logic to destroy a user and their associated resources
+        
     end
 
     private
 
     def user_params
         params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation, :email, :company_id)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
     end
 end
