14a66922ef0ce358fb97002e7ee575a2377ca4a9
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-27 10:19:46 -0700

    change 'before_action' to 'before_filter'

--- app/controllers/concerns/current_user.rb
+++ app/controllers/concerns/current_user.rb
@@ -1,7 +1,7 @@
 module CurrentUser
     extend ActiveSupport::Concern
 
-    before_action :current_user
+    before_filter :current_user
     
     def current_user
         User.find(session[:user_id])
