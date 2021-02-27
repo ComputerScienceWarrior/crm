094bcf1b7799925df5b4067dd30baa34c52171f1
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-27 05:25:27 -0700

    create instance variable assign instance variable '@page_title' based on action.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,14 +1,18 @@
 class StaticController < ApplicationController
     def home
+        @page_title = "Home"
     end
 
     def about
+        @page_title = "About"
     end
     
     def services
+        @page_title = "Services"
     end
 
     def help
+        @page_title = "Help"
     end
 
 end
