aac6805f4311523c97a5cf7884db60b6dd26ff98
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-04 11:25:52 -0700

    remove 'has_many :cases' association.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,8 @@
 class Client < ApplicationRecord
     belongs_to :company
-    has_many :cases
     has_many :notes
     has_many :client_contacts
+    # has_many :cases, through: :user # is this valid / necessary?
     has_many :users, through: :notes
     has_many :users, through: :cases
 end
