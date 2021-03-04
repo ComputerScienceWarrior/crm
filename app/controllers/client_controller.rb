54225e6734cd662be57ff92f305cbfd8cac9bb96
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-04 11:01:10 -0700

    create #cilent_params and #find_client private instance methods. Create before_action call to find_client.

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,11 +1,12 @@
 class ClientController < ApplicationController
 
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
     def index
         @clients = Client.all
     end
 
     def show
-        @client = Client.find(params[:id])
     end
 
     def new
@@ -17,7 +18,6 @@ class ClientController < ApplicationController
     end
 
     def edit
-        @client = Client.find(params[:id])
     end
 
     def update
@@ -26,5 +26,15 @@ class ClientController < ApplicationController
 
     def destroy
         # logic to destroy a client and it's associations
+    end
+
+    private 
+
+    def client_params
+        params.require(:client).permit(:name, :business, :size, :industry, :company_id)
+    end
+
+    def find_client
+        @client = Client.find(params[:id])
     end
 end
