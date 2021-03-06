b49e8e6170e6b58820740e2858e14ab52f317543
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-06 07:36:00 -0700

    Add #accepts_nested_attributes_for instance method on ClientContacts.

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,6 +2,8 @@ class Client < ApplicationRecord
     belongs_to :company
     has_many :notes
     has_many :client_contacts
+    accepts_nested_attributes_for :client_contacts,
+                                reject_if lambda{ |attrs| attrs.first_name.blank?}
     # has_many :cases, through: :user # is this valid / necessary?
     has_many :users, through: :notes
     has_many :users, through: :cases
