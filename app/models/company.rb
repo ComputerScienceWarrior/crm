aefb92a6787a36b914391775522a923f4f54a584
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-10 09:09:47 -0700

    add #accepts_nested_attributes_for call for user

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,13 @@
 class Company < ApplicationRecord
     has_many :clients
     has_many :users
+    accepts_nested_attributes_for :user, reject_if: proc { |attributes| 
+                                                            attributes['username'].blank? || 
+                                                            attributes['firstname'].blank? ||
+                                                            attributes['lastname'].blank? ||
+                                                            attributes['password'].blank? ||
+                                                            attributes['company_id'].blank? 
+                                                        }
     has_many :client_contacts, through: :clients
     has_many :notes, through: :users
     has_many :cases, through: :users
