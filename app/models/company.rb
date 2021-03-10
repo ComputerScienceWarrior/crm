a1d9901bcd9e8689f37d91a7307306ebde35d512
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-10 13:21:58 -0700

    Fix logic error in call to '#accepts_nested_attributes_for :user'. Add validations for a business to have name, size and industry filled in.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,14 +1,11 @@
 class Company < ApplicationRecord
     has_many :clients
     has_many :users
-    accepts_nested_attributes_for :user, reject_if: proc { |attributes| 
-                                                            attributes['username'].blank? || 
-                                                            attributes['firstname'].blank? ||
-                                                            attributes['lastname'].blank? ||
-                                                            attributes['password'].blank? ||
-                                                            attributes['company_id'].blank? 
-                                                        }
+    accepts_nested_attributes_for :users
     has_many :client_contacts, through: :clients
     has_many :notes, through: :users
     has_many :cases, through: :users
+
+    validates_presence_of :name, :industry, :size, {message: " cannot be left blank."}
+    validates :users, presence: true
 end
