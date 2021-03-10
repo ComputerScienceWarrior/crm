36e983a82074108389b0bcfc56ccbb73f25338ec
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-10 09:10:59 -0700

    Add redirect to company_path upon user login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,7 +8,7 @@ class SessionController < ApplicationController
         @user = User.find_by(username: params[:user][:username])
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
-            redirect_to root_path
+            redirect_to company_path(Company.find(@user.company_id))
         else
             render :login
         end
