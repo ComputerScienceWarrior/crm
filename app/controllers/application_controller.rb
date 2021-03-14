8ef5bed9df6598d0aa0f7c64730edea30520a77d
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-14 13:46:34 -0700

    Add determine if user if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :is_guest?
     helper_method :current_company
+    helper_method :is_logged_in?
 
     def current_user
         User.find(session[:user_id])
@@ -10,6 +11,14 @@ class ApplicationController < ActionController::Base
     def is_guest?
         if session[:user_id].nil?
             redirect_to root_path
+        end
+    end
+
+    def is_logged_in?
+        if session[:user_id].nil?
+            false
+        else
+            true
         end
     end
 
