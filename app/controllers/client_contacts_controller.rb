174675761abddfe8af2039f683202e0d50c36b3c
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-15 15:48:29 -0700

    create #find_client method and reference in before_action call.

--- app/controllers/client_contacts_controller.rb
+++ app/controllers/client_contacts_controller.rb
@@ -1,6 +1,6 @@
 class ClientContactsController < ApplicationController
     before_action :find_client_contact, only: [:show, :edit, :update, :destroy]
-
+    before_action :find_client, only: [:new, :create]
     def index
         @client_contacts = ClientContact.all
     end
@@ -14,7 +14,7 @@ class ClientContactsController < ApplicationController
 
     def create
         @client_contact = ClientContact.new(client_contact_params)
-        byebug
+        @client_contact.client_id = @client.id
         if @client_contact.save
             redirect_to client_client_contact_path(@client, @client_contact)
         else
@@ -36,10 +36,14 @@ class ClientContactsController < ApplicationController
     private
 
     def client_contact_params
-        params.require(:client_contact).permit(:first_name, :last_name, :email, :phone, :title)
+        params.require(:client_contact).permit(:first_name, :last_name, :email, :phone, :title, :client_id)
     end
 
     def find_client_contact
         @client_contact = ClientContact.find(params[:id])
+    end
+
+    def find_client
+        @client = Client.friendly.find(params[:client_id])
     end
 end
