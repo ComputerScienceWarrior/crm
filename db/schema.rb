1cc25c12fb3c7a165b40a3ee485f186b064517fe
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-17 10:56:27 -0700

    remove 'cases' table from db

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 2021_03_15_193518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cases", force: :cascade do |t|
-    t.integer "number"
-    t.text "reason"
-    t.boolean "resolved"
-    t.integer "user_id"
-    t.integer "client_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "client_contacts", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
