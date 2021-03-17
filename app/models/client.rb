b1178b3269aee18eec29058cca5d63359dc660fd
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-17 10:20:07 -0700

    Add #validates_presence_of call to :name, :size, :industry and :client_contacts

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,9 +2,11 @@ class Client < ApplicationRecord
     belongs_to :company
     has_many :notes
     has_many :client_contacts
-    accepts_nested_attributes_for :client_contacts
     has_many :users, through: :notes
-    validates :client_contacts, presence: true
+    accepts_nested_attributes_for :client_contacts
+    
+    validates_presence_of :name, :size, :industry, :client_contacts
+    
     extend FriendlyId
     friendly_id :name, use: :slugged
 end
