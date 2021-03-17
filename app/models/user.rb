d9d09b84706be3deeffa3b552ec697e65262b2e1
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-17 09:33:10 -0700

    Add Validation for users attributes and validations for password size to be 8 - 20 characters.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
     belongs_to :company
     has_many :notes
     has_many :clients, through: :notes
+
+    validates_presence_of :first_name, :last_name, :username, :email, {message: " cannot be left blank."}
+    validates :password, length: { in: 8..20 }
+    
     has_secure_password
 end
