987dd8a01480ee3cb1e7a0bbbbfedd7b92695dc5
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-17 10:23:44 -0700

    Add custom message option to #validates_presence_of call. Add validation for size attribute to only be numeric.

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,7 +5,8 @@ class Client < ApplicationRecord
     has_many :users, through: :notes
     accepts_nested_attributes_for :client_contacts
     
-    validates_presence_of :name, :size, :industry, :client_contacts
+    validates_presence_of :name, :size, :industry, :client_contacts, {message: " cannot be left blank."}
+    validates :size, numericality: { only_integer: true }
     
     extend FriendlyId
     friendly_id :name, use: :slugged
