197206acdff46e783c3daacb9b32f604023308d0
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-17 09:35:51 -0700

    Add validations for note attributes and validation for maximum content length of 500 chars.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,6 @@
 class Note < ApplicationRecord
     belongs_to :user
     belongs_to :client
+    validates_presence_of :subject, :content, :users, :client, {message: " cannot be empty."}
+    validates :content, length: { maximum: 500, too_long: "%{count} characters is the maximum allowed!" }
 end
