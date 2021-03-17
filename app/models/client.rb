a5dec9d5128983c5c86735482e7a11d357459085
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-17 02:25:59 -0700

    add #validates call to client_contacts

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,6 +4,7 @@ class Client < ApplicationRecord
     has_many :client_contacts
     accepts_nested_attributes_for :client_contacts
     has_many :users, through: :notes
+    validates :client_contacts, presence: true
     extend FriendlyId
     friendly_id :name, use: :slugged
 end
