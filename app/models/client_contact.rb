7c77aaca016ae78a4c4c04344752483a80eac2d3
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-17 10:39:57 -0700

    Add validations for email uniqueness. add call to #validates_presence_of to client_contacts attributes

--- app/models/client_contact.rb
+++ app/models/client_contact.rb
@@ -1,3 +1,6 @@
 class ClientContact < ApplicationRecord
     belongs_to :client
+
+    validates_presence_of :first_name, :last_name, :email, :phone, :title, {message: " cannot be left blank."}
+    validates :email, uniqueness: true
 end
