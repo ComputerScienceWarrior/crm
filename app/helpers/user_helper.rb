cbefcaed9440cfd050f8085817d719a8dbcdf84b
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-19 20:24:11 -0700

    Add admin? helper to prepare to display admin user functionality

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,9 @@
 module UserHelper
+    def admin?
+        if current_user.is_admin?
+            content_tag(:p, "ADMIN ACCOUNT")
+        else
+            content_tag(:p, "Normal User Account")
+        end
+    end
 end
