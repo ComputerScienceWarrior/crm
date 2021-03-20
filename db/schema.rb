bb2c24994167e60af7997908b59c33ab6c08ae62
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-19 19:21:36 -0700

    Add new columns is_admin?, google_token, and google_refresh_token to users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_135551) do
+ActiveRecord::Schema.define(version: 2021_03_20_021957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,9 @@ ActiveRecord::Schema.define(version: 2021_03_19_135551) do
     t.integer "company_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "google_token"
+    t.string "google_refresh_token"
+    t.boolean "is_admin?", default: false
   end
 
 end
