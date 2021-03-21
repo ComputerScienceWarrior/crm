8cf9d6586d409ac74903b0b3edd683a5e977b902
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-20 19:52:08 -0700

    Create helper method admin_client_functions.

--- app/helpers/client_helper.rb
+++ app/helpers/client_helper.rb
@@ -1,2 +1,9 @@
 module ClientHelper
+    def admin_client_functions
+        if current_user.is_admin?
+            content_tag(:a, "Delete Client Account", href: client_path(@client), method: "delete", data: {confirm: "Are you sure you want to remove #{@client.name} client account??"})
+        else
+            content_tag(:p, "Normal User Account")
+        end
+    end
 end
