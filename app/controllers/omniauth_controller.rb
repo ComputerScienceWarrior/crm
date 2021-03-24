50b8e6fd77c911405a0e37e3d9804e32862dc1d4
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-24 16:15:11 -0700

    Create omniauth controller

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,2 +1,17 @@
-class OmniauthController < ApplicationController
+class OmniauthController < Devise::OmniauthCallbacksController
+
+    def google_oauth2
+        @user = User.create_from_provider_data(request.env["omniauth.auth"])
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to new_user_registration_url
+        end
+    end
+    
+    def failure
+        flash[:error] = "There was an error signing into the page, please try again later."
+        redirect_to new_user_registration_url
+    end
 end
