ed47af334b8078356e84abdf9c5577c945655403
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-24 18:47:30 -0700

    Add logic for google omniauth

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,18 +1,40 @@
 class SessionController < ApplicationController
     
-    def login
-        @user = User.new
-    end
+    # def login
+    #     @user = User.new
+    # end
     
-    def create
-        @user = User.find_by(username: params[:user][:username])
-        if @user && @user.authenticate(params[:user][:password])
+    # def create
+    #     @user = User.find_by(username: params[:user][:username])
+    #     if @user && @user.authenticate(params[:user][:password])
+    #         session[:user_id] = @user.id
+    #         session[:company_id] = @user.company.id
+    #         redirect_to company_path(Company.find(@user.company_id))
+    #     else
+    #         render :login
+    #     end
+    # end
+
+    def google_oauth2
+         # Get access tokens from the google server
+         byebug
+         access_token = request.env["omniauth.auth"]
+         @user = User.from_omniauth(access_token)
+        #  login_path(@user)
+ 
+         # Access_token is used to authenticate request made from the rails application to the google server
+         @user.google_token = access_token.credentials.token
+ 
+         # Refresh_token to request new access_token
+         # Note: Refresh_token is only sent once during the first request
+         refresh_token = access_token.credentials.refresh_token
+         @user.google_refresh_token = refresh_token if refresh_token.present?
+         if @user.save!
             session[:user_id] = @user.id
-            session[:company_id] = @user.company.id
-            redirect_to company_path(Company.find(@user.company_id))
-        else
-            render :login
-        end
+            redirect_to user_path(@user)
+         else
+            render :new
+         end
     end
 
     def destroy
