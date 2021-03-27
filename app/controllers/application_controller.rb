8db23ae53bdd0fd410f5d3e585f24cceb1cfa5c4
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-27 06:06:20 -0700

    remove the #current_user override causing a bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,16 @@
 class ApplicationController < ActionController::Base
     include DeviseWhitelist
     helper_method :current_user
-    helper_method :is_guest?
     helper_method :current_company
-
-    def current_user
-        User.find(session[:user_id])
-    end
+    helper_method :is_guest?
 
     def is_guest?
-        if session[:user_id].nil?
+        if !user_signed_in?
             redirect_to root_path
         end
     end
 
     def current_company
-        Company.find(session[:company_id])
+        current_user.company
     end
 end
