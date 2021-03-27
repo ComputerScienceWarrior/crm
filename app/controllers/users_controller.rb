a02779a0b5598af9e065e2f7b04283493e8a2bd9
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-27 06:20:44 -0700

    change user_controller.rb to users_controller.rb. delete session_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
     before_action :find_user, only: [:index, :show, :edit, :update, :destroy]
 
     def index
