20dfc50d02a992facd8f838601eb0a125d6d8bd2
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-03-27 10:43:53 -0700

    create title_header view helper to display copmany name, or CRM name, based on user login status.

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,2 +1,10 @@
 module StaticHelper
+    def title_header
+        if user_signed_in?
+            # content_tag(:a, "Home", href: company_path(current_user.company), class: "nav-item")
+            content_tag(:h1, current_company.name, id: "h1-header")
+        else
+            content_tag(:h1, "Swift Solutions CRM", id: "h1-header")
+        end
+    end
 end
